ee9cb73b6e1080f9052b85d8d91fd935378e8852
FoX_2129 <fox2129@gmail.com>
2021-03-09 08:46:04 +0300

    first commit

---
+++ Ruby1.rb
@@ -0,0 +1,8 @@
+puts 'Привет! Напиши пожалуйста, как тебя зовут?'
+FirstName = gets
+puts 'Какое твоё отчество?'
+MidleName = gets
+puts 'Фамилия?'
+SecondName = gets
+
+#puts 'Хорошо. Теперь я буду обращаться к тебе ' + FirstName + ' '+ MidleName + ' ' + SecondName + '.'
