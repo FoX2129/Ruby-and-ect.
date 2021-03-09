b0c8be415a68d2ee8b97020423e6da1d8ddb6723
FoX_2129 <fox2129@gmail.com>
2021-03-09 08:51:18 +0300

    second commit

---
+++ Ruby2.rb
@@ -0,0 +1,19 @@
+puts 'Добрый день. Пожалуйста, введите своё любимое число:'
+
+i=0
+while i == 0
+  a = gets.chomp
+if a == a.swapcase
+  if a==''
+   puts 'Я подожду...'
+  else
+    b = a.to_i+1
+    puts 'Ваше любимое число - ' + a
+    puts 'Возможно, вам понравится число ' + b.to_s
+    i=1
+  end
+  else
+  puts 'Возможно, вы ввели не число. Попробуйте ещё раз:'
+end#if a == a.swapcase
+
+end#while
