3e1d7556b5a6217881744a90b58049770798044b
FoX_2129 <fox2129@gmail.com>
2021-03-10 09:42:36 +0300

    RubyA

--- Ruby2.rb
+++ Ruby2.rb
@@ -4,19 +4,11 @@ loop do
     if a==0
       puts 'Я подожду...'
     else
-      b = a.to_i+1
-      puts 'Ваше любимое число - ' + a
-      puts 'Возможно, вам понравится число ' + b.to_s
-      i=1
-    end#if a==''
-  else#if a == a.swapcase
-    puts 'Возможно, вы ввели не число. Попробуйте ещё раз:'
-  end#if a == a.swapcase
       puts "Ваше любимое число - #{a}\nВозможно, вам понравится число #{a+1}"
-     
       break
     end#if a == a.swapcase
 end
+
 
 #  a = 5
 #  puts "#{a.class}"
