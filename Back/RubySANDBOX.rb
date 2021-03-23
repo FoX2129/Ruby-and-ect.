c42edb1282eab88edb99b7c2ae996f20aeae8c51
FoX_2129 <fox2129@gmail.com>
2021-03-23 13:32:32 +0300

    Backing stuff. #Issue 7256

---
+++ Back/RubySANDBOX.rb
@@ -0,0 +1,16 @@
+#git add Ruby4.rb
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/FoX2129/Ruby-and-ect..git
+#или        git push
+#FoX2129
+#Arcady112100
+
+#def sayMoo # скажи: "Му"
+#puts 'мууууууу...'
+#end
+
+loop do
+  a = gets
+  puts a.class
+end
---
+++ RubySANDBOX.rb
@@ -0,0 +1,51 @@
+#git add Ruby4.rb
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/FoX2129/Ruby-and-ect..git
+#или        git push
+#FoX2129
+#Arcady112100
+
+#def sayMoo # скажи: "Му"
+#puts 'мууууууу...'
+#end
+
+#git checkout main
+#git branch
+#git add Name.type
+#git commit -m "Test"
+#git push -u origin develop
+
+#git commit --amend -m "обновленный комментарий к коммиту"
+# Отредактируйте файлы hello.py и main.py
+# git add hello.py git commit
+# Вспомните, что вы забыли добавить изменения из файла main.py
+# git add main.py git commit --amend --no-edit
+
+# sqlite3
+# rails server
+
+# a = [ 1, 'cat', 3.14 ] # массив из трех элементов
+# a[0] # обращение к первому элементу (1)
+# a[2] = nil
+# теперь массив имеет следующий вид: [ 1, 'cat', nil ]
+# a = %w{ муравей пчела кот собака лось } - массив из слов.
+
+# def say_goodnight(name)
+# result = 'Спокойной ночи, ' + name
+# return result
+# end
+# puts say_goodnight('Капиталист')
+# /home/fox2129/projects/rails/demo/app/views/products/show.html.erb
+# /home/fox2129/projects/rails/demo/app/views/products/index.html.erb
+# demo/app/controllers/products_controller.rb
+# http://localhost:3000/store/index
+
+# depot> rake db:rollback
+
+a="Хрен с маслом"
+if a =~ /\040/
+  puts "da"
+else
+  puts  "net"
+end
