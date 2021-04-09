efd68b666357e72e38f1ba15f4f7b00f4e604edd
FoX_2129 <fox2129@gmail.com>
2021-04-09 09:59:26 +0300

    SoR. Exceptions and "to_i". #Issue 7258

--- Store/cart.rb
+++ Store/cart.rb
@@ -11,17 +11,16 @@ class Cart
 
   def save_to_file
     File.open("#{@owner}_cart.txt","w") do |f|
-      @items.each {|i| f.puts "#{i.name}:#{i.price}:#{i.weight}:#{i.height}"}
+      @items.each {|i| f.puts i}
     end
   end
 
   def read_from_file
-    return unless File.exists?("#{@owner}_cart.txt")
-    item_fields = File.readlines("#{@owner}_cart.txt") #"name:price:weight:height\n"
-    item_fields.map! {|i| i.chomp } # "car:100:50:1"
-    item_fields.map! {|i| i.split(":")}
-    item_fields.each {|i| @items << RealItem.new(name: i[0], price: i[1].to_i, weight: i[2].to_i, height: i[3].to_i)}
+    File.readlines("#{@owner}_cart.txt").each {|i| @items << i.to_real_item } #"name:price:weight:height\n"
     @items.uniq!
+  rescue Errno::ENOENT
+    File.open("#{@owner}_cart.txt","w") {}
+    puts "File #{@owner}_cart.txt created"
   end
 
 end
