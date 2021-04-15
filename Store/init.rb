676179a75ed5d45151a755ec62004cb9e4736cd7
FoX_2129 <fox2129@gmail.com>
2021-04-15 14:46:19 +0300

    SoR. Domain Specific Language. #Issue 7258

--- Store/init.rb
+++ Store/init.rb
@@ -1,21 +1,28 @@
-require_relative "string"
-require_relative "item_container"
-require_relative "item"
-require_relative "virtual_item"
-require_relative "real_item"
-require_relative "antique_item"
-require_relative "cart"
-require_relative "order"
+require_relative "store_application"
+StoreApplication.config do |app|
+  app.name        = "My Store"
+  app.environment = :production
 
+  app.admin do |admin|
+    admin.email = "admin@mystore.com"
+    admin.login = "admin"
+    admin.send_info_emails_on :mondays
+  end
+
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
 
 @items = []
 #cart.add_item(Item.new({:price => 30,:weight => 3}))
-@items << AntiqueItem.new({:price =>111, :name => "Диван"})
-@items << RealItem.new({:price => 0, :weight => 50, :height => 60, :name => "Стена"})
+@items << AntiqueItem.new(:price => 111, :name => "Диван")
+@items << RealItem.new(price: 0, weight: 50, height: 60, name: "Стена")
 @items << RealItem.new({:price => 105, :weight => 1, :height=> 0.5, :name => "нига"})
 
 cart = Cart.new("Andrey")
 cart.add_item RealItem.new({:price => 0, :weight => 50, :height => 60, :name => "Car"})
 cart.add_item RealItem.new({:price => 1, :weight => 70, :height => 60, :name => "Car"})
 cart.add_item RealItem.new({:price => 105, :weight => 1, :height=> 0.5, :name => "нига"})
-p cart.all_cars
